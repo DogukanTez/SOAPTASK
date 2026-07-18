6e2fda4376b597e1a46d909f67d3062c1613c677
Doğukan Tez <dogukantez7@gmail.com>
2026-07-18 10:16:05 +0300

    project init
    project initialized with java 17, maven and packages of validation, lombok, spring web, spring web services, actuator.

---
+++ src/main/java/com/dogukantez/soaptask/SoaptaskApplication.java
@@ -0,0 +1,13 @@
+package com.dogukantez.soaptask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaptaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoaptaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dogukantez/soaptask/SoaptaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogukantez.soaptask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaptaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
