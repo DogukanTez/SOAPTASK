334b1e1fea15a25808c558cad9300ebb22caf702
Doğukan Tez <dogukantez7@gmail.com>
2026-07-18 11:22:49 +0300

    feat: wsdl file added and Readme file updated.
    Also at InvoiceRequest, validation patterns applied following by the documentation.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SOAPTASK
 
+- WSDL url üzerinden erişmek yerine dosya olarak resources/wsdl içine kopyalandı. Offline olarak çalışabilmesi ve buildin dış servisin erişilebilirliğine bağlı olmaması ve tekrarlanabilir olması için.
+- countryCode ve vatNumber sadece xsd:string olarak tanımlanmış,
+dokümanda ; The countryCode input parameter must follow the pattern [A-Z]{2} ve The vatNumber input parameter must follow the pattern [0-9A-Za-z\+\*\.]{2,12} 
+cümleleri ile belirtilse de, xsd:string olarak tanımlandığı için pattern validation yapılmamaktadır. Bu yüzden format kontrolünü validation ile yaptım.
+
+-WSDL dosyasındaki <wsdlsoap:address> değeri http:// şemasında. 
+-validation paternleri dokümanda belirtilen patternler ile aynı şekilde yazıldı.
--- src/main/java/com/dogukantez/soaptask/dto/InvoiceRequest.java
+++ src/main/java/com/dogukantez/soaptask/dto/InvoiceRequest.java
@@ -1,6 +1,7 @@
 package com.dogukantez.soaptask.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,14 +13,18 @@ public class InvoiceRequest {
     private String invoiceNumber;
 
     @NotBlank
+    @Pattern(regexp = "^[A-Z]{2}$", message = "country code must be ISO format.") // iso format hataları için
     private String sellerCountryCode;
 
     @NotBlank
+    @Pattern(regexp = "^[0-9A-Za-z+*.]{2,12}$", message = "Invalid KDV number format") // kdv paterni dokümanda belirtildiği üzere
     private String sellerVatNumber;
 
     @NotBlank
+    @Pattern(regexp = "^[A-Z]{2}$", message = "country code must be ISO format.")
     private String buyerCountryCode;
 
     @NotBlank
+    @Pattern(regexp = "^[0-9A-Za-z+*.]{2,12}$", message = "Invalid KDV number format")
     private String buyerVatNumber;
 }
